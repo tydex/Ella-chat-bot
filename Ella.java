d6eb9430edbe9e8c1918594b8fe7bc95bc1d91f7
tydex <samuel.odekunle@gmail.com>
2014-11-24 13:31:45 +0000

    Ella.java
    Ella chat bot v0.01

--- Ella.java
+++ Ella.java
@@ -12,18 +12,7 @@ public class Ella{
 		this.name=name;
 	}
 	
-	public static void populate(){
-		phrases[0] = "I'm doing great and you?";
-		phrases[1] = "I'm feeling a bit off today!";
-		phrases[2] = "none of your business";
-		phrases[3] = "I'm Sad....";
-		phrases[4] = "I feel wholesome...kinda like bread";
-	}
-	
 	public static void feelings(){
-		feel[0] = "I sense....You have the spirit of a dog! woof!!";
-		feel[1] = "You remind me of the titanic...a big excuse to sink...lol!";
-		feel[2] = "You remind me of my jeep..vrooooom! ah! feel the wind on my face!";
 	}
 	
 	public static int randomWithRange(int min, int max){
@@ -36,12 +25,37 @@ public class Ella{
 		System.out.println("Hello I'm Ella! and you are?");
 		Scanner in = new Scanner(System.in);
 		String namer = in.nextLine();
-		Ella(namer);
-		populate();
+		
+		
+		namer.trim();
+		if (namer.length()>15){ System.out.println("Your name is kinda long isn't it!"); }
+		
+		
+		System.out.println("and your last name is?");
+		Scanner in2 = new Scanner(System.in);
+		String namer2 = in2.nextLine();
+		
+		namer2.trim();
+		
+		Person ppl = new Person(namer,namer2);
+		
+		if (ppl.known()) {
+			System.out.println("HI "+namer+"! Good to see you again!");
+		}else{
+				System.out.println("I don't think we have met before");
+				try{ 
+					ppl.savePerson("ella.txt");
+					}catch (IOException e ){
+					System.out.println(e);
+				}
+			}
+		
 		feelings();
 		
 		if (namer.equalsIgnoreCase("Ella")){ System.out.println("oh wow! that's my name too!"); 
-		}else System.out.println("hmm...your name is kinda boring. But anyway...");
+		}else System.out.println("Pleased to meet you");
+		
+					
 		
 		
 		System.out.println("em... "+namer +"? wouldn't you like to know how I am?");
@@ -49,7 +63,7 @@ public class Ella{
 		if (resp.equalsIgnoreCase("No")){
 			System.out.println("FINE! then go away!");
 			System.exit(0);
-			}else if (resp.equalsIgnoreCase("maybe")){
+		}else if (resp.equalsIgnoreCase("maybe")){
 				System.out.println(namer+(" it's not a hard question is it?? ... have a good think about it for a few seconds!"));
 				try{
 					Thread.sleep(10000);
@@ -58,19 +72,64 @@ public class Ella{
 				}
 				System.out.println("...Good! Now I'm sure you want to know how if feel so I'll tell you!");
 			}
+		
+		try{ 
+			File file = new File("resp.txt");
+			BufferedReader reader = new BufferedReader(new FileReader(file));
+			String ln = reader.readLine();
+			List<String> lines = new ArrayList<String>();
+			while(ln != null){
+				lines.add(ln);
+				ln = reader.readLine();
+			}
+			Random r = new Random();
+			System.out.println(namer+" "+lines.get(r.nextInt(lines.size())));
+			}catch (IOException e){
+			e.printStackTrace();}
 			
-			 
-		int rresp = randomWithRange(0,4);
+		
+			
 		int rrepB = randomWithRange(0,2);
 		
+		System.out.println(namer +" So how are you today?");
+		String rrsp = in.nextLine();
+		try{
+			FileWriter write = new FileWriter("resp.txt",true);
+			PrintWriter prt = new PrintWriter(write);
 		
-		System.out.println(namer +" "+phrases[rresp]);
-		System.out.println(namer +" "+feel[rrepB]);
+			prt.printf("%s" + "%n", rrsp);
+			prt.close();
+			}catch (IOException e){
+				e.printStackTrace();
+			}
 			
+		System.out.println ("Here's Something to think about...");
+		try{
+					Thread.sleep(5000);
+		}catch (InterruptedException ex){
+					Thread.currentThread().interrupt();
+			}
+		
+		try{ 
+			File file2 = new File("convo.ella");
+			BufferedReader reader2 = new BufferedReader(new FileReader(file2));
+			String ln2 = reader2.readLine();
+			List<String> lines2 = new ArrayList<String>();
+			while(ln2 != null){
+				lines2.add(ln2);
+				ln2 = reader2.readLine();
+			}
+			Random r2 = new Random();
+			System.out.println(namer+", "+lines2.get(r2.nextInt(lines2.size())));
+			}catch (IOException e){
+			e.printStackTrace();}
+		
+				
 		
 		System.exit(0);
 	}
 }
-		
+
+
 		
 		
